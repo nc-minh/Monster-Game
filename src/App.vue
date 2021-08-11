c81f3549fa60fead23470118d19e07f45918ce33
nguyencongminh-dev <nguyencongminh.official@gmail.com>
2021-08-11 14:15:20 +0700

    first commit

---
+++ src/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <div id="app">
+    <div class="figure">
+      <div class="figure__item">
+        <div class="figure__item__name">
+          <h3>You</h3>
+        </div>
+        <div class="figure__item__img">
+          <img src="D:\Projects\Monster\monster_game\src\assets\you.png" alt="" class="figure__item__img--link">
+        </div>
+      </div>
+      <div class="figure__item">
+        <div class="figure__item__name">
+          <h3>Monster</h3>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+  data(){
+    return{
+
+    }
+  },
+  components: {
+
+  }
+}
+</script>
+
+<style>
+
+</style>
